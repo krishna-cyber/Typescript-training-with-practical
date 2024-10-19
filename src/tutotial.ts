02b07d8fd80abf7b0631129bc3866260b863d80e
Krishna Tiwari <61178259+krishna-cyber@users.noreply.github.com>
2024-10-19 19:57:21 +0545

    ts code added

---
+++ src/tutotial.ts
@@ -0,0 +1,59 @@
+console.log(`typescript tutorial`);
+
+// check friend name
+let friendNames: string[];
+
+friendNames = ["ram", "shyam", "hari", "krishna"];
+
+function checkFriend(list: string[]): boolean {
+  return list.includes("hari");
+}
+
+console.log(checkFriend(friendNames));
+
+// check discount or penalty
+
+function checkDiscountOrPenalty(price: number, discount?: number): number {
+  return price - (discount || 0);
+}
+
+checkDiscountOrPenalty(10);
+
+checkDiscountOrPenalty(10, 20);
+
+//with default value
+
+function getDiscount(price: number, discount: number = 0): number {
+  return price - discount;
+}
+
+getDiscount(100);
+getDiscount(100 - 50);
+
+// function with union types
+function processData(data: string | number) {
+  return typeof data == "string" ? data.toUpperCase() : data * 2;
+}
+
+console.log(processData(10));
+
+// employee create by id
+
+function createEmployee({ id }: { id: number }): {
+  id: number;
+  isActive: boolean;
+} {
+  return { id: id, isActive: id%2==0 };
+}
+
+console.log(createEmployee({ id: 20 }));
+
+
+// another example without destructuring
+
+function welcomeStudent(student:{id:number,name:string}):void{
+
+    console.log(`Welcome student of roll ${student.id} and name ${student.name}`)
+}
+
+welcomeStudent({id:10,name:"krishna Tiwari"})
